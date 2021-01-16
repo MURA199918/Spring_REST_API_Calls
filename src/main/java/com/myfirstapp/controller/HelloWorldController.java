87aad0c82d14db33968093e274bbcd7e9aad1090
MURA199918 <muralisaranr18@gmail.com>
2021-01-16 09:58:51 +0530

    [Murali]Refactored to create REST Controller to demonstrate various HTTP methods and respond hello messages to user

--- src/main/java/com/myfirstapp/controller/HelloWorldController.java
+++ src/main/java/com/myfirstapp/controller/HelloWorldController.java
@@ -1,4 +1,13 @@
 package com.myfirstapp.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hellocontroller")
 public class HelloWorldController {
+    @RequestMapping(value = {"", "/", "/home"})
+    public String sayHello() {
+        return "Hello World!!!";
+    }
 }
