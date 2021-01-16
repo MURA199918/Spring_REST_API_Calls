5193943f660d5cb8d85e333cf16464774e4411d1
MURA199918 <muralisaranr18@gmail.com>
2021-01-16 10:20:17 +0530

    [Murali]Refactored to use GET request method and pass name as query parameter

--- src/main/java/com/myfirstapp/controller/HelloWorldController.java
+++ src/main/java/com/myfirstapp/controller/HelloWorldController.java
@@ -1,13 +1,20 @@
 package com.myfirstapp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hellocontroller")
+@RequestMapping("/hello")
 public class HelloWorldController {
     @RequestMapping(value = {"", "/", "/home"})
     public String sayHello() {
         return "Hello World!!!";
+    }
+
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name") String name) {
+        return "Hello " + name + "!";
     }
 }
